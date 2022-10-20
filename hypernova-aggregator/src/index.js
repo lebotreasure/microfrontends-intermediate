6a4d0cff5ef7345b4dc1694e89de12e1b92534d1
lebohangkhaeane <khaeane@thedigitalacademy.co.za>
2022-10-20 23:09:41 +0200

    Document header

--- hypernova-aggregator/src/index.js
+++ hypernova-aggregator/src/index.js
@@ -17,7 +17,7 @@ app.get('/', async (req, res) => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
+      <title>Hypernova Microfrontends</title>
       <link rel="stylesheet" href="/style.css">
     </head>
     <body>
